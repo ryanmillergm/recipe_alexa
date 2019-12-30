97ab9f18db7e7cdfee46b27dda175aab0d8c2e22
Ryan Miller <ryanmillergm@gmail.com>
2019-12-30 16:40:27 -0700

    Adds intent request handler

--- app/handlers/intent_request_handler.rb
+++ app/handlers/intent_request_handler.rb
@@ -1,2 +1,35 @@
+require 'sinatra'
+require 'json'
+require 'pry'
+require './app/services/recipe_service'
+require './app/formatters/message_formatter'
+
 class IntentRequestHandler
+  attr_reader :json_response
+
+  def initialize(json_response)
+    @json_response = json_response
+  end
+
+  def intent
+    json_response["request"]["intent"]["name"]
+  end
+
+  def value
+    json_response["request"]["intent"]["slots"]["name"]["value"]
+  end
+
+  def route_intents
+    if intent == "find_recipe"
+      recipe_service = RecipeService.new
+      recipe_service.recipes(value)
+      binding.pry
+    #   message_formatter = MessageFormatter.new
+    # elsif intent == "recipe"
+    #   recipe_service = RecipeService.new
+    # elsif intent = "step"
+    #   recipe_service = RecipeService.new
+    end
+  end
+
 end
