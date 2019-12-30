09a30e53588a768afc9aba8e6a17054fd495a496
Ryan Miller <ryanmillergm@gmail.com>
2019-12-30 16:41:26 -0700

    Adds recipe service

--- app/services/recipe_service.rb
+++ app/services/recipe_service.rb
@@ -1,2 +1,21 @@
+require 'sinatra'
+require 'json'
+require 'pry'
+require 'faraday'
+
 class RecipeService
+  def conn
+    Faraday.new(url: 'http://localhost:3000') do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def parsed_response(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def recipes(food)
+    binding.pry
+    parsed_response(conn.get("/api/v1/recipes"))
+  end
 end
