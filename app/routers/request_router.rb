472d16db064fa03f31d75df9fc2a8fe7ca209ad8
Ryan Miller <ryanmillergm@gmail.com>
2020-01-03 17:32:21 -0700

    Adds browser to control chrome

--- app/routers/request_router.rb
+++ app/routers/request_router.rb
@@ -5,18 +5,19 @@ require './app/responses/launch_response'
 require './app/handlers/intent_request_handler'
 
 class RequestRouter
-  attr_reader :json_response
+  attr_reader :json_response, :browser
 
-  def initialize(json_response)
+  def initialize(json_response, browser)
     @json_response = json_response
+    @browser = browser
   end
 
   def format_response
     if json_response["request"]["type"] == "LaunchRequest"
-      launch_response = LaunchResponse.new
+      launch_response = LaunchResponse.new(@browser)
       launch_response.render_response
     elsif json_response["request"]["type"] == "IntentRequest"
-      IntentRequestHandler.new(json_response).route_intents
+      IntentRequestHandler.new(json_response, @browser).route_intents
     end
   end
 end
